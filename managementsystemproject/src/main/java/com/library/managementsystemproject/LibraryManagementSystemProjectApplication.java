822fdb8d09fe68fed61be18ab237696382df2745
Gupta <sparsh.gupta@acsicorp.com>
2023-01-16 15:14:56 +0530

    initial commit

--- managementsystemproject/src/main/java/com/library/managementsystemproject/LibraryManagementSystemProjectApplication.java
+++ managementsystemproject/src/main/java/com/library/managementsystemproject/LibraryManagementSystemProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagementsystemprojectApplication {
+public class LibraryManagementSystemProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManagementsystemprojectApplication.class, args);
+		SpringApplication.run(LibraryManagementSystemProjectApplication.class, args);
 	}
 
 }
