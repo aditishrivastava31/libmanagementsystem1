dd6bb7a3512d55b51b2420ae7933662b9c719585
aditishrivastava31 <117710038+aditishrivastava31@users.noreply.github.com>
2023-01-16 12:41:08 +0530

    Add files via upload

---
+++ managementsystemproject/src/main/java/com/library/managementsystemproject/ManagementsystemprojectApplication.java
@@ -0,0 +1,13 @@
+package com.library.managementsystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementsystemprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementsystemprojectApplication.class, args);
+	}
+
+}
---
+++ managementsystemproject/src/test/java/com/library/managementsystemproject/ManagementsystemprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.managementsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsystemprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
