6875c990b615f088d7492c939509b67991fa2418
Anoj14 <anojanu1414@gmail.com>
2023-01-19 04:11:53 -0800

    entities and repositories are updated

--- managementsystemproject/src/main/java/com/library/managementsystemproject/repositories/AuthorRepository.java
+++ managementsystemproject/src/main/java/com/library/managementsystemproject/repositories/AuthorRepository.java
@@ -1,12 +1,18 @@
 package com.library.managementsystemproject.repositories;
 
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.library.managementsystemproject.entities.Category;
+import com.library.managementsystemproject.entities.Author;
 
 
-public interface CategoryRepo extends JpaRepository<Category, Long> {
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
 	
-
+	
+	
 }
