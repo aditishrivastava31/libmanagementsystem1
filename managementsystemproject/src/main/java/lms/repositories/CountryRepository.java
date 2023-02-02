b71b2ac230728707af8acf193ce836e03f343761
jarvis5797 <ashutosh.baranwal@acsicorp.com>
2023-02-02 11:59:20 +0530

    Created user and admin signup API

---
+++ managementsystemproject/src/main/java/lms/repositories/CountryRepository.java
@@ -0,0 +1,18 @@
+package lms.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lms.entities.Country;
+
+/**
+ * this interface will be used to connect {@link Country} from database
+ * @author ashutosh.baranwal , sparsh.gupta
+ *
+ */
+
+public interface CountryRepository extends JpaRepository<Country, Long>{
+	
+	Country findByCountryName(String countryName);
+	
+}
+
