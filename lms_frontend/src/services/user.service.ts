1284eb60eb343bf003507fa9dee58a5c286f3c0b
jarvis5797 <ashutosh.baranwal@acsicorp.com>
2023-02-21 10:56:54 +0530

    saving user with address

--- lms_frontend/src/services/user.service.ts
+++ lms_frontend/src/services/user.service.ts
@@ -8,8 +8,28 @@ export class UserService {
 
   constructor(private http:HttpClient) { }
 
-  public addUser(user:any){
-    return this.http.post('${baseUrl}/user/signUp',user);
+  public addUser(user:any , country: any , state: any , city: any){
+    return this.http.post('http://localhost:8080/user/signUp',user, {params:{
+      countryname: country,
+      statename: state,
+      cityname : city
+    }});
+  }
+
+  public getcountry(){
+    return this.http.get('http://localhost:8080/country');
+  }
+
+  public getstate(country:any){
+    return this.http.get('http://localhost:8080/state',{params:{
+      countryname: country
+    }})
+  }
+
+  public getcity(state:any){
+    return this.http.get('http://localhost:8080/city',{params:{
+      statename: state
+    }})
   }
 
 }
